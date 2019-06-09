c8550a87db24afa60d6440f0eb7298c7190266de
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-06-09 21:12:20 +0100

    UI intergraation tests passing

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,67 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should update the display with the result of arithmetical operations', function () {
+    running_total = element (by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4');
+  })
+
+  it('should be able to chain multiple opertators', function () {
+    running_total = element (by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8');
+  })
+
+  it('should be able to calculate negative numbers', function () {
+    running_total = element (by.css('#running_total'));
+    element(by.css('#number8')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-2');
+  })
+
+  it('should be able to calculate decimal numbers', function () {
+    running_total = element (by.css('#running_total'));
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.5');
+  })
+
+  it('should be able to calculate large numbers', function () {
+    running_total = element (by.css('#running_total'));
+    element(by.css('#number8')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('89198280');
+  })
+
+  it('should be able to return not NaN when divided by 0', function () {
+    running_total = element (by.css('#running_total'));
+    element(by.css('#number8')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('NaN')
+  })
+
 });
